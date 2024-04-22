6c4614b32ce1959d4474954fc4c948f08a28852e
gameboo <alexandre.joannou@gmail.com>
2024-04-22 11:55:07 +0100

    Generalised vm image generation script to run from an arbitrary location

--- vm-image/gen-vm-image.sh
+++ vm-image/gen-vm-image.sh
@@ -1,5 +1,11 @@
-curl -C- -O https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
-mv jammy-server-cloudimg-amd64.img jammy-server-cloudimg-amd64.qcow2
-qemu-img resize jammy-server-cloudimg-amd64.qcow2 32G
-mkisofs --output cloud-init/seed.iso -volid cidata -joliet -rock cloud-init/user-data cloud-init/meta-data
-kvm -m 2048 -machine q35 -drive file=jammy-server-cloudimg-amd64.qcow2,if=virtio -drive driver=raw,file=cloud-init/seed.iso,if=virtio -nographic
+#! /usr/bin/env sh
+
+WORKDIR=$(pwd)
+VMIMAGEDIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+curl https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img --output $WORKDIR/de10pro-playground-$USER-vm.qcow2
+qemu-img resize $WORKDIR/de10pro-playground-$USER-vm.qcow2 16G
+ISO_IMG=$(mktemp /tmp/XXXXXX.iso)
+mkisofs --output $ISO_IMG -volid cidata -joliet -rock $VMIMAGEDIR/cloud-init/user-data $VMIMAGEDIR/cloud-init/meta-data
+kvm -m 2048 -machine q35 -drive file=$WORKDIR/de10pro-playground-$USER-vm.qcow2,if=virtio -drive driver=raw,file=$ISO_IMG,if=virtio -nographic
+rm $ISO_IMG
